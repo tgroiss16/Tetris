80231bd2e91361b861568e40dfd3783c92a992b4
4AreLife <david.engelberger@gmail.com>
2019-05-09 10:06:59 +0200

    Buttons zu Activity oder wie tobi sagen würde Activitiy

--- app/src/main/java/com/example/tetris/MainActivity.java
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tetris;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.main_playgame_button);
+        Button btn2 = (Button)findViewById(R.id.main_statistics_button);
+        Button btn3 = (Button)findViewById(R.id.main_button_options);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StatisticsActivity.class));
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OptionsActivity.class));
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/tetris/OptionsActivity.java
@@ -0,0 +1,25 @@
+package com.example.tetris;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class OptionsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+
+        Button btn = (Button)findViewById(R.id.options_back_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OptionsActivity.this, MainActivity.class));
+            }
+        });
+
+
+    }
+}
