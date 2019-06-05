354cc39d2ff6f28ffdf32cb078bb518395905d76
tgroiss16 <tgroisz16@gmail.com>
2019-06-05 16:45:17 +0200

    2 New Activitys

---
+++ app/src/main/java/com/example/tetris/LoseActivity.java
@@ -0,0 +1,63 @@
+package com.example.tetris;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoseActivity extends AppCompatActivity {
+    int score = 0;
+    int lines = 0;
+    MediaPlayer mediaPlayer = MainActivity.mediaPlayer;
+    static public boolean musicboolean = OptionsActivity.musicboolean;
+    static public boolean turnboolean = OptionsActivity.turnboolean;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameover);
+        Button btn = findViewById(R.id.endgame_button);
+        SharedPreferences pref = this.getSharedPreferences("recentscores", Context.MODE_PRIVATE);
+        score =  pref.getInt("score", 2);
+        lines =  pref.getInt("lines",3);
+
+        TextView tlines = findViewById(R.id.endgame_linesclearde);
+        tlines.setText(lines+"");
+        TextView tscore =  findViewById(R.id.endgame_score);
+        tscore.setText(""+score);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoseActivity.this, MainActivity.class));
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!mediaPlayer.isPlaying() && !musicboolean)
+        {
+            mediaPlayer.start();
+            mediaPlayer.setLooping(true);
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+    }
+
+}
